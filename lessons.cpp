9c7b367d992abcae5e736de896f983c230dc09d6
Deus134 <niania33303@gmail.com>
2024-11-21 20:01:47 +0300

    Добавьте файлы проекта.

---
+++ lessons.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T> // template func for permitation of var values
+void SwapVariables(T& firstVar, T& secondVar)
+{
+	T temp = firstVar;
+	firstVar = secondVar;
+	secondVar = temp;
+}
+
+void main()
+{
+	string firstVariable, secondVariable; // declaring variables
+
+	cout << "Enter the first variable: "; 
+	cin >> firstVariable;		// entering the first variable
+
+	cout << "Enter the second variable: ";
+	cin >> secondVariable;		// entering the second variable
+	cout << endl;
+
+	cout << "now value of the first variable will be value of second variable and vice versa" << endl << endl;
+
+	SwapVariables(firstVariable, secondVariable); // calling the SwapVariable func
+
+	cout << "value of first variable = " << firstVariable << endl;		//  output of the value of the first variable
+	cout << "value of second variable = " << secondVariable << endl;	//  output of the value of the second variable
+}
